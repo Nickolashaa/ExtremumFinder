83aa73733c1bac42c72a3cc0e867398e39414568
Nickolashaa <gracevnikolaj220@gmail.com>
2025-04-11 23:57:44 +0300

    no commit

--- ExtremumFinder.py
+++ ExtremumFinder.py
@@ -35,10 +35,10 @@ class ExtremumFinder:
         else:
             def comparison(a, b):
                 return a > b
-        x_sr = (a + b) / 2
         L = abs(b - a)
-        f_x_sr = self.eval(x_sr)
         while L > e:
+            x_sr = (a + b) / 2
+            f_x_sr = self.eval(x_sr)
             y = a + abs(L) / 4
             z = b - abs(L) / 4
             f_y = self.eval(y)
@@ -69,27 +69,46 @@ class ExtremumFinder:
 
     
     def GoldenRatioMethod(self, a, b, e, findmin):
+        base_interval = {
+            "a": a,
+            "b": b,
+        }
+        f_a = self.eval(a)
+        f_b = self.eval(b)
         if findmin:
-            y = a + ((3 - 5 ** 0.5) / 2) * (b - a)
-            z = a + b - y
-            delta = None
-            while delta is None or delta > e:
-                f_y = self.eval(y)
-                f_z = self.eval(z)
-                if f_y <= f_z:
-                    b = z
-                    z = y
-                    y = a + b - y
-                else:
-                    a = y
-                    y = z
-                    z = a + b - z
-                
-                delta = abs(a - b)
-            result = f"x* принадлежит [{a:.5f}, {b:.5f}]\nx* = {(a + b) / 2:.5f}"
-            return result
+            def comparison(a, b):
+                return a <= b
         else:
-            pass
+            def comparison(a, b):
+                return a > b
+        y = a + ((3 - 5 ** 0.5) / 2) * (b - a)
+        z = a + b - y
+        delta = None
+        while delta is None or delta > e:
+            f_y = self.eval(y)
+            f_z = self.eval(z)
+            if comparison(f_y, f_z):
+                b = z
+                z = y
+                y = a + b - y
+            else:
+                a = y
+                y = z
+                z = a + b - z
+            
+            delta = abs(a - b)
+            
+        x_ans = (a + b) / 2
+        f_x_ans = self.eval(x_ans)
+        
+        if comparison(f_a, f_x_ans):
+            x_ans = base_interval["a"]
+            f_x_ans = f_a
+        if comparison(f_b, f_x_ans):
+            x_ans = base_interval["b"]
+            f_x_ans = f_b
+        result = f"x* = {x_ans:.5f}\nf(x*) = {f_x_ans:.5f}"
+        return result
     
     def NewFibonacci(self, array):
         elem = array[-1] + array[-2]
@@ -97,39 +116,57 @@ class ExtremumFinder:
         return elem
 
     def FibonacciNumberMethod(self, a, b, e, findmin):
+        base_interval = {
+            "a": a,
+            "b": b,
+        }
+        f_a = self.eval(a)
+        f_b = self.eval(b)
         if findmin:
-            l = 2 * e
-            L0 = abs(b - a)
-            Fib = [1, 1]
-            while Fib[-1] < L0 / l:
-                self.NewFibonacci(Fib)
-            N = len(Fib) - 1
-            y = a + (Fib[N-2] / Fib[N]) * (b - a)
-            z = a + (Fib[N-1] / Fib[N]) * (b - a)
-            f_y = self.eval(y)
-            f_z = self.eval(z)
-            for k in range(N - 2):
-                if f_y <= f_z:
-                    b = z
-                    z = y
-                    y = a + (Fib[N-k-3] / Fib[N-k-1]) * (b - a)
-                    f_z = f_y
-                    f_y = self.eval(y)
-                else:
-                    a = y
-                    y = z
-                    z = a + (Fib[N-k-2] / Fib[N-k-1]) * (b - a)
-                    f_y = f_z
-                    f_z = self.eval(z)
-            z = y + e
-            f_z = self.eval(z)
-            
-            if f_y < f_z:
+            def comparison(a, b):
+                return a <= b
+        else:
+            def comparison(a, b):
+                return a > b
+        l = 2 * e
+        L0 = abs(b - a)
+        Fib = [1, 1]
+        while Fib[-1] < L0 / l:
+            self.NewFibonacci(Fib)
+        N = len(Fib) - 1
+        y = a + (Fib[N-2] / Fib[N]) * (b - a)
+        z = a + (Fib[N-1] / Fib[N]) * (b - a)
+        f_y = self.eval(y)
+        f_z = self.eval(z)
+        for k in range(N - 2):
+            if comparison(f_y, f_z):
                 b = z
+                z = y
+                y = a + (Fib[N-k-3] / Fib[N-k-1]) * (b - a)
+                f_z = f_y
+                f_y = self.eval(y)
             else:
                 a = y
-            
-            result = f"x* принадлежит [{a:.5f}, {b:.5f}]\nx* = {(a + b) / 2:.5f}"
-            return result
+                y = z
+                z = a + (Fib[N-k-2] / Fib[N-k-1]) * (b - a)
+                f_y = f_z
+                f_z = self.eval(z)
+        z = y + e
+        f_z = self.eval(z)
+        
+        if comparison(f_y, f_z):
+            b = z
         else:
-            pass
+            a = y
+            
+        x_ans = (a + b) / 2
+        f_x_ans = self.eval(x_ans)
+        
+        if comparison(f_a, f_x_ans):
+            x_ans = base_interval["a"]
+            f_x_ans = f_a
+        if comparison(f_b, f_x_ans):
+            x_ans = base_interval["b"]
+            f_x_ans = f_b
+        result = f"x* = {x_ans:.5f}\nf(x*) = {f_x_ans:.5f}"
+        return result
